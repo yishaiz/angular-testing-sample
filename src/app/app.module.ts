0797fdccdda6ee35d481e301111e3595406e79ab
yishai <yishai5555@gmail.com>
2017-04-25 11:27:57 +0300

    'b1'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/banner/banner.component.spec.ts
@@ -0,0 +1,137 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { DebugElement } from '@angular/core'
+
+import { BannerComponent } from  './banner.component';
+
+
+describe('BannerComponent - 123456', () => {
+
+  let comp : BannerComponent;
+  let fixture : ComponentFixture<BannerComponent>;
+  let de : DebugElement;
+  let el : HTMLElement;
+
+  console.log('banner spec start');
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BannerComponent]
+    });
+
+    fixture = TestBed.createComponent(BannerComponent);
+
+    comp = fixture.componentInstance;
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+  });
+
+
+  it('should display original title', () => {
+    fixture.detectChanges();
+
+    console.log(el.textContent);
+    console.log(comp.title);
+
+    expect(el.textContent).toContain(comp.title);
+
+    // expect(true).toBeTruthy();
+
+    // throw new Error('error check');
+  });
+
+
+  it('should display a different test title', () => {
+    comp.title = 'Test Title';
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Test Title');
+  });
+
+
+});
+
+
+/*
+ describe('BannerComponent', () => {
+
+ let comp : BannerComponent;
+ let fixture : ComponentFixture<BannerComponent>;
+ let de : DebugElement;
+ let el : HTMLElement;
+
+ console.log('banner spec start')
+
+ beforeEach(() => {
+ TestBed.configureTestingModule({
+ declarations: [BannerComponent]
+ });
+
+ fixture = TestBed.createComponent(BannerComponent);
+
+ comp = fixture.componentInstance;
+
+ // query for the title <h1> by CSS element selector
+ de = fixture.debugElement.query(By.css('h1'));
+ el = de.nativeElement;
+
+ //
+ throw new Error('s');
+
+ });
+
+ });
+ */
+
+
+
+/*
+
+ describe('BannerComponent', ()=>{
+
+ let comp: BannerComponent;
+ let fixture : ComponentFixture<BannerComponent>;
+ let de: DebugElement;
+ let el : HTMLElement;
+
+ });
+
+ */
+
+
+/*
+ describe('BannerComponent', ()=>{
+
+ });
+ */
+
+
+/*
+
+ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+ import { BannerComponent } from './banner.component';
+
+ describe('BannerComponent', () => {
+ let component: BannerComponent;
+ let fixture: ComponentFixture<BannerComponent>;
+
+ beforeEach(async(() => {
+ TestBed.configureTestingModule({
+ declarations: [ BannerComponent ]
+ })
+ .compileComponents();
+ }));
+
+ beforeEach(() => {
+ fixture = TestBed.createComponent(BannerComponent);
+ component = fixture.componentInstance;
+ fixture.detectChanges();
+ });
+
+ it('should create', () => {
+ expect(component).toBeTruthy();
+ });
+ });
+ */
